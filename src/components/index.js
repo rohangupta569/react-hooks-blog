76dc1aca49d437062b200885d1162d5f7697f66f
Rohan Gupta <rohangupta569@gmail.com>
2022-08-12 00:52:57 +0530

    Setting up routes

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import App from './App';
+import CreatePost from './CreatePost';
+import Navbar from './Navbar';
+import Home from './Home';
+import PostDetail from './PostDetail';
+
+export { App, Navbar, Home, CreatePost, PostDetail };
